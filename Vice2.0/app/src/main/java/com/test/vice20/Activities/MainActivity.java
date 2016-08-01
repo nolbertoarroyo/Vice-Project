8210b29437b78723f735126be6e51403558f8310
Kitty Tang <Kittys.Tang@gmail.com>
2016-08-01 11:46:58 -0700

    api interface and methods added

--- Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
+++ Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
@@ -1,9 +1,13 @@
-package com.test.vice20;
+package com.test.vice20.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.test.vice20.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String baseURL = "http://vice.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Vice2.0/app/src/main/java/com/test/vice20/Models/Item.java
+++ Vice2.0/app/src/main/java/com/test/vice20/Models/Item.java
@@ -3,10 +3,12 @@ package com.test.vice20.Models;
 /**
  * Created by kitty on 8/1/16.
  */
-import java.util.ArrayList;
-import java.util.List;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Item {
 
