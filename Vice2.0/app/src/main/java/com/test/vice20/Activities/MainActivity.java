38627db6a23d457637bb1c82e2d24033c3645a53
sterlingann <sterlingann@gmail.com>
2016-08-02 13:42:42 -0700

    added xmls, drawer fragment, updated main activity, inserted icon into drawables

--- Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
+++ Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
@@ -1,17 +1,43 @@
-package com.test.vice20.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+//still working on main activity, 8/2 1:38
+
+package package com.test.vice20;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.test.vice20.R;
+import java.util.ArrayList;
+import java.util.List;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static String baseURL = "http://vice.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        private void configureDrawer(ArrayList<Boolean> chkd) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarhere);
+            setSupportActionBar(toolbar);
+            if (getSupportActionBar()!= null) {
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+
+            List<OpenDrawer> drawerEntries = new ArrayList<>();
+
+            drawerEntries.add(new DrawerContent(arrayhere[0]));
+            drawerEntries.add(new Separator());
+            drawerEntries.add(new Toggle(arrayhere[2]));
+            drawerEntries.add(new Toggle(arrayhere[3]));
+            drawerEntries.add(new Toggle(arrayhere[4]));
+            drawerEntries.add(new Toggle(arrayhere[5]));
+
+
+            DrawerFragment drawerFragment = (DrawerFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.drawerfrag);
+
+        }
     }
 }
