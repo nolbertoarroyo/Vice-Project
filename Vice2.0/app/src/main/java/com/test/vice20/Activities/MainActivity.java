cde60a9fda6ad40660b49640de29bc50833821af
Nolberto Arroyo <nolberto.arroyo@gmail.com>
2016-08-02 09:41:40 -0700

    set layout back to activity_main

--- Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
+++ Vice2.0/app/src/main/java/com/test/vice20/Activities/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_details);
+        setContentView(R.layout.activity_main);
     }
 }
